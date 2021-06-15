854abc03835bc485d7ee0b9605f71744f406d513
Baiim <sektia7@gmail.com>
2021-06-15 21:34:43 +0700

    First commit

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,39 @@
+      <section class="section section-lg bg-gray-900">
+        <div class="container">
+          <div class="row row-30">
+            <div class="col-xs-10 col-lg-4 align-self-center"><a class="brand" width="30px" href="/"><img class="brand-logo-dark"  src="images/logo-svg.svg" srcset="images/logo-svg.svg" alt="Voelas"/><img class="brand-logo-light" src="images/logo-svg-2.svg" srcset="images/logo-svg-2.svg" alt="FAST"/></a></div>
+            <div class="col-xs-10 col-sm-6 col-lg-4">
+              <h5><span class="big font-weight-sbold">Event detail</span></h5>
+              <div class="event-detail">
+                <p class="event-detail-time big">
+                  <time data-splitting datetime="2019-01-05">January 5 to 9, 2019</time>
+                </p>
+                <p class="event-detail-address big" data-splitting>Kampus Universitas Pelita Bangsa, Jl. Inspeksi Kalimalang No.9, Cibatu, Cikarang Sel., Bekasi, Jawa Barat 17530 </p> <span><a class="event-detail-link" href="https://goo.gl/maps/59zpZ4sWrg5N4WmLA" target="_blank">Lihat di MAP</a></span> <br>
+                <p class="event-detail-address big" data-splitting>Gedung Dinas Pariwisata Kabupaten Bekasi, Sertajaya, Kec. Cikarang Tim., Bekasi, Jawa Barat 17530</p><span><a class="event-detail-link" href="https://goo.gl/maps/rGp3TrTGfr72jijC7" target="_blank">Lihat di MAP</a></span>
+
+              </div>
+            </div>
+            <div class="col-xs-10 col-sm-6 col-lg-4">
+              <h5><span class="big font-weight-sbold">Social Updates</span></h5>
+              <div class="event-detail">
+                <p class="big">Jangan lewatkan update menarik seputer FAST UPB<br class="d-none d-xl-block"> Ikuti media social kami.
+                </p>
+                <ul class="list-inline list-inline-xs">
+                  <li><a class="icon icon-rect  fa-instagram" href="#" data-triangle=".icon-rect-overlay">
+                      <div class="icon-rect-overlay"></div></a> &nbsp; IG: @fastupb</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- Divider-->
+      <div class="divider divider-gray-900 text-center"></div>
+      <!-- Footer Classic-->
+      <footer class="section footer-classic context-dark">
+        <div class="container">
+          <p class="rights"><span>Copyright&nbsp;</span><span>&copy;&nbsp;</span><span class="copyright-year"></span><span>&nbsp;</span><span>by</span> <a href="http://bit.ly/dan-fisher">Dan Fisher</a>
+          </p>
+        </div>
+      </footer>
+    </div>
