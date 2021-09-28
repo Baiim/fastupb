de0037654754c2cf8137e67026d5acb290bb1d23
Baiim <sektia7@gmail.com>
2021-09-28 18:01:23 +0700

    Update logo sponsor

--- resources/views/includes/sponsor.blade.php
+++ resources/views/includes/sponsor.blade.php
@@ -8,12 +8,12 @@
               <div>
                 <!-- Sponsor--><a class="sponsor" href="#" data-triangle=".sponsor-overlay">
                   <div class="sponsor-overlay"></div>
-                  <div class="sponsor-img"><img src="images/sponsor-01-117x70.png" alt="" width="117" height="70"/>
+                  <div class="sponsor-img"><img src="images/sponsor/dewa-1.png" alt="" width="500" height="70"/>
                   </div></a>
               </div>
             </div>
           </div>
-          <div class="col-sm-5 col-lg-3">
+          {{-- <div class="col-sm-5 col-lg-3">
             <div class="wow">
               <div>
                 <!-- Sponsor--><a class="sponsor" href="#" data-triangle=".sponsor-overlay">
@@ -83,7 +83,7 @@
                 </div>
               </div>
             </div>
-        </div>
+        </div> --}}
       </div>
       <div class="container">
           <h3>Media Partner</h3>
@@ -93,7 +93,7 @@
                 <div>
                   <!-- Sponsor--><a class="sponsor" href="#" data-triangle=".sponsor-overlay">
                     <div class="sponsor-overlay"></div>
-                    <div class="sponsor-img"><img src="images/sponsor-01-117x70.png" alt="" width="117" height="70"/>
+                    <div class="sponsor-img"><img src="images/medpart/medpart-1.png" alt="" width="117" height="70"/>
                     </div></a>
                 </div>
               </div>
@@ -103,7 +103,7 @@
                 <div>
                   <!-- Sponsor--><a class="sponsor" href="#" data-triangle=".sponsor-overlay">
                     <div class="sponsor-overlay"></div>
-                    <div class="sponsor-img"><img src="images/sponsor-02-66x83.png" alt="" width="66" height="83"/>
+                    <div class="sponsor-img"><img src="images/medpart/medpart-2.jpeg" alt="" width="66" height="83"/>
                     </div></a>
                 </div>
               </div>
@@ -113,7 +113,7 @@
                 <div>
                   <!-- Sponsor--><a class="sponsor" href="#" data-triangle=".sponsor-overlay">
                     <div class="sponsor-overlay"></div>
-                    <div class="sponsor-img"><img src="images/sponsor-03-119x33.png" alt="" width="119" height="33"/>
+                    <div class="sponsor-img"><img src="images/medpart/medpart-3.jpeg" alt="" width="119" height="33"/>
                     </div></a>
                 </div>
               </div>
@@ -123,7 +123,7 @@
                 <div>
                   <!-- Sponsor--><a class="sponsor" href="#" data-triangle=".sponsor-overlay">
                     <div class="sponsor-overlay"></div>
-                    <div class="sponsor-img"><img src="images/sponsor-04-116x68.png" alt="" width="116" height="68"/>
+                    <div class="sponsor-img"><img src="images/medpart/medpart-4.jpeg" alt="" width="116" height="68"/>
                     </div></a>
                 </div>
               </div>
@@ -133,7 +133,7 @@
                   <div>
                     <!-- Sponsor--><a class="sponsor" href="#" data-triangle=".sponsor-overlay">
                       <div class="sponsor-overlay"></div>
-                      <div class="sponsor-img"><img src="images/sponsor-01-117x70.png" alt="" width="117" height="70"/>
+                      <div class="sponsor-img"><img src="images/medpart/medpart-5.png" alt="" width="117" height="70"/>
                       </div></a>
                   </div>
                 </div>
@@ -143,7 +143,7 @@
                   <div>
                     <!-- Sponsor--><a class="sponsor" href="#" data-triangle=".sponsor-overlay">
                       <div class="sponsor-overlay"></div>
-                      <div class="sponsor-img"><img src="images/sponsor-02-66x83.png" alt="" width="66" height="83"/>
+                      <div class="sponsor-img"><img src="images/medpart/medpart-6.png" alt="" width="66" height="83"/>
                       </div></a>
                   </div>
                 </div>
@@ -153,7 +153,7 @@
                   <div>
                     <!-- Sponsor--><a class="sponsor" href="#" data-triangle=".sponsor-overlay">
                       <div class="sponsor-overlay"></div>
-                      <div class="sponsor-img"><img src="images/sponsor-03-119x33.png" alt="" width="119" height="33"/>
+                      <div class="sponsor-img"><img src="images/medpart/medpart-7.png" alt="" width="119" height="33"/>
                       </div></a>
                   </div>
                 </div>
@@ -163,12 +163,117 @@
                   <div>
                     <!-- Sponsor--><a class="sponsor" href="#" data-triangle=".sponsor-overlay">
                       <div class="sponsor-overlay"></div>
-                      <div class="sponsor-img"><img src="images/sponsor-04-116x68.png" alt="" width="116" height="68"/>
+                      <div class="sponsor-img"><img src="images/medpart/medpart-8.png" alt="" width="116" height="68"/>
+                      </div></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-5 col-lg-3">
+                <div class="wow">
+                  <div>
+                    <!-- Sponsor--><a class="sponsor" href="#" data-triangle=".sponsor-overlay">
+                      <div class="sponsor-overlay"></div>
+                      <div class="sponsor-img"><img src="images/medpart/medpart-9.png" alt="" width="116" height="68"/>
+                      </div></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-5 col-lg-3">
+                <div class="wow">
+                  <div>
+                    <!-- Sponsor--><a class="sponsor" href="#" data-triangle=".sponsor-overlay">
+                      <div class="sponsor-overlay"></div>
+                      <div class="sponsor-img"><img src="images/medpart/medpart-10.png" alt="" width="116" height="68"/>
+                      </div></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-5 col-lg-3">
+                <div class="wow">
+                  <div>
+                    <!-- Sponsor--><a class="sponsor" href="#" data-triangle=".sponsor-overlay">
+                      <div class="sponsor-overlay"></div>
+                      <div class="sponsor-img"><img src="images/medpart/medpart-11.png" alt="" width="116" height="68"/>
+                      </div></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-5 col-lg-3">
+                <div class="wow">
+                  <div>
+                    <!-- Sponsor--><a class="sponsor" href="#" data-triangle=".sponsor-overlay">
+                      <div class="sponsor-overlay"></div>
+                      <div class="sponsor-img"><img src="images/medpart/medpart-12.png" alt="" width="116" height="68"/>
+                      </div></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-5 col-lg-3">
+                <div class="wow">
+                  <div>
+                    <!-- Sponsor--><a class="sponsor" href="#" data-triangle=".sponsor-overlay">
+                      <div class="sponsor-overlay"></div>
+                      <div class="sponsor-img"><img src="images/medpart/medpart-13.png" alt="" width="116" height="68"/>
                       </div></a>
                   </div>
                 </div>
               </div>
           </div>
         </div>
+        <div class="container">
+            <h3>Support</h3>
+            <div class="row row-30 row-lg-50 justify-content-center">
+              <div class="col-sm-5 col-lg-3">
+                <div class="wow">
+                  <div>
+                    <!-- Sponsor--><a class="sponsor" href="#" data-triangle=".sponsor-overlay">
+                      <div class="sponsor-overlay"></div>
+                      <div class="sponsor-img"><img src="images/support/support-1.jpeg" alt="" width="119" height="70"/>
+                      </div></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-5 col-lg-3">
+                <div class="wow">
+                  <div>
+                    <!-- Sponsor--><a class="sponsor" href="#" data-triangle=".sponsor-overlay">
+                      <div class="sponsor-overlay"></div>
+                      <div class="sponsor-img"><img src="images/support/support-2.jpg" alt="" width="119" height="83"/>
+                      </div></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-5 col-lg-3">
+                <div class="wow">
+                  <div>
+                    <!-- Sponsor--><a class="sponsor" href="#" data-triangle=".sponsor-overlay">
+                      <div class="sponsor-overlay"></div>
+                      <div class="sponsor-img"><img src="images/support/support-3.jpg" alt="" width="119" height="33"/>
+                      </div></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-5 col-lg-3">
+                <div class="wow">
+                  <div>
+                    <!-- Sponsor--><a class="sponsor" href="#" data-triangle=".sponsor-overlay">
+                      <div class="sponsor-overlay"></div>
+                      <div class="sponsor-img"><img src="images/support/support-4.png" alt="" width="116" height="68"/>
+                      </div></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-5 col-lg-3">
+                  <div class="wow">
+                    <div>
+                      <!-- Sponsor--><a class="sponsor" href="#" data-triangle=".sponsor-overlay">
+                        <div class="sponsor-overlay"></div>
+                        <div class="sponsor-img"><img src="images/support/support-5.png" alt="" width="117" height="70"/>
+                        </div></a>
+                    </div>
+                  </div>
+                </div>
+            </div>
+          </div>
     </div>
   </section>
